704d7930601a90fccb35df3de82d25b9fa48500c
Joe Khoury <joe.el.k97@gmail.com>
2015-10-03 14:01:50 -0400

    Move functions for the AI

--- Python/botplayer/PlayerAI.py
+++ Python/botplayer/PlayerAI.py
@@ -10,3 +10,19 @@ class PlayerAI:
     def get_move(self, gameboard, player, opponent):
         # Write your AI here.
         return Move.NONE
+
+    def move_right(self):
+        Move.FACE_RIGHT
+        MOVE.FORWARD
+
+    def move_left(self):
+        Move.FACE_LEFT
+        Move.FORWARD
+
+    def move_up(self):
+        Move.FACE_UP
+        Move.FORWARD
+
+    def move_down(self):
+        Move.FACE_DOWN
+        Move.FORWARD
