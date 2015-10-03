1ab0c561b9bf408ac2f9d7577329381920a03d9a
Joe Khoury <joe.el.k97@gmail.com>
2015-10-03 13:40:17 -0400

    Moved the file
    Because of importing troubles.

---
+++ Python/PythonClientAPI/libs/PlayerAI.py
@@ -0,0 +1,12 @@
+from libs.Game.Enums import *
+from libs.Game.MapOutOfBoundsException import *
+
+
+class PlayerAI:
+    def __init__(self):
+        # Initialize any objects or variables you need here.
+        pass
+
+    def get_move(self, gameboard, player, opponent):
+        # Write your AI here.
+        return Move.NONE
